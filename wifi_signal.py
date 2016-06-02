1d5848939b4661dbccf6dfa8a67e18e38c34a998
Paween Theanvanichpant <paween.the@gmail.com>
2016-06-02 12:13:54 +0700

    Change format of data.csv

--- wifi_signal.py
+++ wifi_signal.py
@@ -1,6 +1,6 @@
 from wifi import Cell, Scheme
-import time
 import csv
+import arrow
 
 print "Entering wifi-signal.py"
 
@@ -8,14 +8,22 @@ SSID = "MU-WiFi"
 csv_file = "/home/pi/Desktop/wifi-signal-meter/data.csv"
 
 cells = Cell.all('wlan0')
-unix_time = time.time()
+time = arrow.now().isoformat()
 
-for cell in cells:
-    if cell.ssid == SSID:
-        with open(csv_file, 'a') as f:
-            writer = csv.writer(f)
-            # ssid,signal,quality,frequency,unix_time
-            writer.writerow([cell.ssid, cell.signal, cell.quality, cell.frequency, unix_time])
-            print "Write to data.csv"
+found = False
+
+# ssid,signal,quality,frequency,datetime
+
+with open(csv_file, 'a') as f:
+    writer = csv.writer(f)
+    for cell in cells:
+        if cell.ssid == SSID:
+            found = True
+            print "Found MU-WiFi"
+        writer.writerow([cell.ssid, cell.signal, cell.quality, cell.frequency, time])
+
+    if not found:
+        print "Not found MU-WiFi"
+        writer.writerow([SSID, '0', '0', '0', time])
 
 print "Exit wifi-signal.py"
