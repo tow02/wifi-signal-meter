ed6e27812061bcda42639338676ae0ff82927a3c
Paween Theanvanichpant <paween.the@gmail.com>
2016-06-03 19:34:22 +0700

    Use snapshot csv file to dropbox. Need to gzip csv file

--- backup_data.py
+++ backup_data.py
@@ -1,24 +1,22 @@
 # Include the Dropbox SDK
 import dropbox
+import arrow
+import gzip
 from secret import access_token
 
 client = dropbox.client.DropboxClient(access_token)
-print 'linked account: ', client.account_info()
+# print 'linked account: ', client.account_info()
+
+time = arrow.now().isoformat()
+
+work_directory = '/home/pi/Desktop/wifi-signal-meter/'
+source_filename = '/home/pi/Desktop/wifi-signal-meter/data.csv'
+destination_filename = '/icns171/' + time + '.csv'
 
 try:
-    client.file_delete('/icns171/backup_data.csv')
-except Exception as e:
-    print e
-
-try:
-    client.file_move('/icns171/data.csv', '/icns171/backup_data.csv')
-except Exception as e:
-    print e
-
-try:
-    f = open('/home/pi/Desktop/wifi-signal-meter/data.csv', 'r')
-    response = client.put_file('/icns171/data.csv', f)
-    print "uploaded:", response
-    f.close()
+    f_csv = open(source_filename, 'r')
+    response = client.put_file(destination_filename, f_csv)
+    # print "uploaded:", response
+    f_csv.close()
 except Exception as e:
     print e
